d7946dedf7388101add9740985d360aaaeff4b1f
d2d.weizhi <d2d.weizhi@gmail.com>
2025-07-27 17:50:01 +0800

    Conditional Rendering for Due Date

--- src/components/ui/shared/assignment-card.tsx
+++ src/components/ui/shared/assignment-card.tsx
@@ -105,7 +105,7 @@ export default observer(function AssignmentCard({
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-1" viewBox="0 0 20 20"
                   fill="currentColor">
                   <path fillRule="evenodd"
-                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
                     clipRule="evenodd"></path>
                 </svg>
                 <span className="text-sm text-gray-500">Submitted on May 18, 2023</span>
